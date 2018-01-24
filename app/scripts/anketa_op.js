e2628d26e58680fe9667bb1cfcf9009c02c242f7
Allegathor <allegathor@gmail.com>
2018-01-24 21:02:17 +0300

    Task 2

---
+++ app/scripts/anketa_op.js
@@ -0,0 +1,50 @@
+var btn = document.querySelector('.js-data-btn');
+
+var formatData = function(userData) {
+	var formatted = {};
+
+	var ageNum = parseInt(userData.age, 10);
+
+	formatted.main = 'ФИО: ' + userData.name + ' ' + userData.surname + ' ' + userData.patr;
+	formatted.ageYears = 'Возраст, лет: ' + userData.age;
+	formatted.ageDays = 'Возраст, дней: ' + (ageNum * 365);
+
+	formatted.gender = (userData.gender === true) ? 'Пол: мужской' : 'Пол: женский';
+
+	(userData.gender) ?
+	(formatted.retirement = (ageNum >= 61) ? 'Пенсионный возраст: да' : 'Пенсионный возраст: нет') :
+	(formatted.retirement = (ageNum >= 56) ? 'Пенсионный возраст: да' : 'Пенсионный возраст: нет');
+
+	alert(formatted.main + '\n' + formatted.ageYears + '\n' + formatted.ageDays + '\n' + formatted.gender + '\n' + formatted.retirement);
+
+}
+
+var requestData = function() {
+	var user = {};
+
+	user.name = prompt('Введите имя', '');
+	if (user.name === null) { return; }
+
+	user.surname = prompt('Введите фамилию', '');
+	if (user.surname === null) { return; }
+
+	user.patr = prompt('Введите отчество', '');
+	if (user.patr === null) { return; }
+
+	user.age = prompt('Введите возраст', '');
+	if (user.age === null) { return; }
+
+	user.gender = confirm('Вы мужчина?')
+	if (user.gender === null) { return; }
+
+	if (user) {
+		formatData(user);
+	}
+
+}
+
+requestData();
+
+btn.addEventListener('click', function(evt){
+	requestData();
+})
