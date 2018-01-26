9322405c64921c8ae5d146723c5a7001781525e0
Allegathor <allegathor@gmail.com>
2018-01-27 00:33:27 +0300

    Prime nums

--- app/scripts/prime-numbers.js
+++ app/scripts/prime-numbers.js
@@ -1,10 +1,19 @@
 var findPrimeNums = function(startIndex, stopIndex) {
 	var count = 0;
+
 	for(var i = startIndex; i <= stopIndex; i++) {
-		if(i !== 0 && i !== 1 && i & 1) {
-			count++;
-			console.log(i);
-		}
+
+		var k = 1;
+		do {
+
+			k++;
+
+			if(k === i) {
+				console.log(i);
+				count++;
+			}
+
+		} while(i % k && k < i);
 	}
 
 	document.querySelector('.js-prime-nums').innerText = 'Общее количество простых чисел: ' + count;
