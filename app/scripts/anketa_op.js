48f33d33fbd4506b8f150b841deea4e8a4805893
Allegathor <allegathor@gmail.com>
2018-02-07 15:46:37 +0300

    N.03

--- app/scripts/anketa_op.js
+++ app/scripts/anketa_op.js
@@ -1,50 +1,102 @@
-var btn = document.querySelector('.js-data-btn');
+var ready = function() {
+	var btn = document.querySelector('.js-data-btn');
 
-var formatData = function(userData) {
-	var formatted = {};
+	var formatData = function(userData) {
+		var main;
 
-	var ageNum = parseInt(userData.age, 10);
+		var ageNum = parseInt(userData.age.value, 10);
+		var ageDays;
+		var ageInFive;
+		var gender;
+		var retirement;
 
-	formatted.main = 'ФИО: ' + userData.name + ' ' + userData.surname + ' ' + userData.patr;
-	formatted.ageYears = 'Возраст, лет: ' + userData.age;
-	formatted.ageDays = 'Возраст, дней: ' + (ageNum * 365);
+		main = 'Ваше ФИО: ' + userData.name.value + ' ' + userData.surname.value + ' ' + userData.patr.value;
+		ageYears = 'Ваш возраст в годах: ' + userData.age.value;
+		ageDays = 'Ваш возраст в днях: ' + (ageNum * 365);
+		ageInFive = 'Через пять лет вам будет: ' + (ageNum + 5);
 
-	formatted.gender = (userData.gender === true) ? 'Пол: мужской' : 'Пол: женский';
+		gender = (userData.gender.value === true) ? 'Пол: мужской' : 'Пол: женский';
 
-	(userData.gender) ?
-	(formatted.retirement = (ageNum >= 61) ? 'Пенсионный возраст: да' : 'Пенсионный возраст: нет') :
-	(formatted.retirement = (ageNum >= 56) ? 'Пенсионный возраст: да' : 'Пенсионный возраст: нет');
+		(gender) ?
+		(retirement = (ageNum >= 61) ? 'Вы на пенсии: да' : 'Вы на пенсии: нет') :
+		(retirement = (ageNum >= 56) ? 'Вы на пенсии: да' : 'Вы на пенсии: нет');
 
-	alert(formatted.main + '\n' + formatted.ageYears + '\n' + formatted.ageDays + '\n' + formatted.gender + '\n' + formatted.retirement);
+		alert(main + '\n' + ageYears + '\n' + ageDays + '\n' + ageInFive + '\n' + gender + '\n' + retirement);
 
-}
-
-var requestData = function() {
-	var user = {};
-
-	user.name = prompt('Введите имя', '');
-	if (user.name === null) { return; }
-
-	user.surname = prompt('Введите фамилию', '');
-	if (user.surname === null) { return; }
-
-	user.patr = prompt('Введите отчество', '');
-	if (user.patr === null) { return; }
-
-	user.age = prompt('Введите возраст', '');
-	if (user.age === null) { return; }
-
-	user.gender = confirm('Вы мужчина?')
-	if (user.gender === null) { return; }
-
-	if (user) {
-		formatData(user);
 	}
 
+	var isValid = function(value, type) {
+		var toIntAttempt = parseInt(value, 10);
+		if (value !== '') {
+			if ( type === 'string' && isNaN(toIntAttempt) ) {
+				return true;
+
+			} else if ( type === 'number' && !isNaN(toIntAttempt) ) {
+				return true;
+
+			} else {
+				return false;
+			}
+		}
+
+		console.log('fep');
+		return false;
+
+	}
+
+	var canShowNext  = function(prop) {
+		var expType;
+		var fail;
+		var failMsg;
+
+		if (!prop.confirm) {
+
+			expType = (!prop.num) ? 'string' : 'number';
+			fail = false;
+			failMsg = (!prop.num) ? ' корректно! (поле не должно быть пустым; используйте кириллицу/латиницу; числовые значения не допускаются)' : ' корректно! (поле не должно быть пустым, а введёное значение должно являться числом)';
+
+			while (!isValid(prop.value, expType)) {
+				prop.value = (!fail) ? prompt(prop.msg, '') :
+				prompt(prop.msg + failMsg, '');
+
+				if (prop.value === null) { return false; }
+				fail = true;
+			}
+
+		} else {
+			prop.value = confirm(prop.msg);
+			if (prop.value === null) { return false; }
+
+		}
+
+		return true;
+	}
+
+	var getData = function() {
+		var user = {};
+
+		user.name 		= { msg:'Введите имя', value: '' };
+		user.surname	= { msg: 'Введите фамилию', value: '' };
+		user.patr			= { msg: 'Введите отчество', value: ''};
+		user.age			= { msg: 'Введите возраст', value: '', num: true };
+		user.gender		= { msg: 'Ваш пол — мужской? (Ок — да, отмена — нет)', value: 0, confirm: true };
+
+		for (prop in user) {
+			var res = canShowNext(user[prop]);
+			if (!res) {
+				return;
+			}
+		}
+
+		formatData(user);
+
+	}
+
+	getData();
+
+	btn.addEventListener('click', function(evt){
+		getData();
+	})
 }
 
-requestData();
-
-btn.addEventListener('click', function(evt){
-	requestData();
-})
+window.addEventListener('load', ready)
