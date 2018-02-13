13d7245226e5ae17b69f7922cf474b4b75745f02
Allegathor <allegathor@gmail.com>
2018-02-14 01:23:10 +0300

    Add recursive variant for isPalindrome function

--- app/scripts/palindrome.js
+++ app/scripts/palindrome.js
@@ -1,7 +1,7 @@
 var btn = document.getElementById('check-btn');
 var input = document.getElementById('text-area');
 
-var searchPalindrome = function(string) {
+var isPalindrome = function(string) {
 
   var s = string.toLowerCase();
 
@@ -35,8 +35,40 @@ var searchPalindrome = function(string) {
   }
 }
 
+
+var isPalindromeRecur = function(string) {
+
+  var s = string.toLowerCase();
+  var ch1;
+  var ch2;
+
+  s = s.replace(/[\n\u0020\u2013\u2014\-?!'".,ьъ]/g, '');
+  s = s.replace(/ё/g, 'е');
+
+  return (function isEqual(i, j) {
+    i = i || 0;
+    j = j || s.length - 1;
+
+    ch1 = s[i];
+    ch2 = s[j];
+
+    if (i < j) {
+      if (ch1 === ch2) {
+        return isEqual(i + 1, j - 1);
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  })();
+
+}
+
+
 var btnHandler = function() {
-  console.log(searchPalindrome(input.value));
+  console.log(isPalindrome(input.value));
+  console.log(isPalindromeRecur(input.value));
 }
 
 btn.addEventListener('click', btnHandler);
